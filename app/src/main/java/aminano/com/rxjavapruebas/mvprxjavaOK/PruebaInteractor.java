3c17342061970a178e62c05d552a8721069d8c20
aminano <aminano@droiders.com>
2016-03-14 18:02:44 +0100

    add all types support

--- app/src/main/java/aminano/com/rxjavapruebas/mvprxjavaOK/PruebaInteractor.java
+++ app/src/main/java/aminano/com/rxjavapruebas/mvprxjavaOK/PruebaInteractor.java
@@ -15,32 +15,9 @@ import rx.schedulers.Schedulers;
  * @author Antonio Miñano
  */
 public class PruebaInteractor<T  extends Observable> extends InteractorInterface{
-  private Observable<SuperObject> objectObservable;
-
-  //public Observable<T> action() {
   public T action() {
-    Log.e("solucion asdasd","");
-    Post post=new Post();
-    post.setId(1111);
-    SuperObject sp=(SuperObject)post;
-    Log.e("solucion superobject", ""+((Post)sp).getId());
-    ////Handler handlerTimer = new Handler();
-    ////handlerTimer.postDelayed(new Runnable() {
-    ////public void run() {
-    //Image a = new Image();
-    //a.setUrl("asd");
-    //a.setHeight(12);
-    //a.setWidth(200);
-    //setValue(a);
-    //objectObservable = getObject();
-    ////}
-    ////}, 2000);
-    //return objectObservable;
     ForumService forum=new ForumService();
     return (T)forum.getApi()
         .getPost(1);
-    //return forum.getApi()
-    //    .getPost(1);
-
   }
 }
